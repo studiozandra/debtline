3e28bd356bdc620915727ae0e32be5a46e5fa527
studiozandra <zan@studiozandra.com>
2019-08-06 20:23:35 -1000

    add detailed comments, make main func reusable, remove NaN output

--- js/app.js
+++ js/app.js
@@ -2,9 +2,10 @@ document.getElementById('amountAndInterest1').style.display = 'none';
 document.getElementById('amountAndInterest2').style.display = 'none';
 document.getElementById('amountAndInterest3').style.display = 'none';
 
-  document.getElementById("add").onclick = function () {
+// event of clicking yellow plus sign to add up to 3 other debt inputs
+document.getElementById("plus-sign").onclick = function () {
 
-     if (document.getElementById('amountAndInterest1').style.display == 'none') {
+    if (document.getElementById('amountAndInterest1').style.display == 'none') {
       document.getElementById('amountAndInterest1').style.display = 'block';
       console.log("one");    
      }else if (document.getElementById('amountAndInterest2').style.display == 'none'){
@@ -14,28 +15,28 @@ document.getElementById('amountAndInterest3').style.display = 'none';
       document.getElementById('amountAndInterest3').style.display = 'block';
       console.log("one and two");
      }
-  };
-  
-document.getElementById("remove1").onclick = function () {
+};
+
+// event of clicking yellow minus sign to remove 3 other debt inputs
+document.getElementById("minus-sign1").onclick = function () {
      if (document.getElementById('amountAndInterest1').style.display == 'block') {
-      console.log("clicked minus")
       document.getElementById('amountAndInterest1').style.display = 'none';    
      }    
 };
 
-document.getElementById("remove2").onclick = function () {
+document.getElementById("minus-sign2").onclick = function () {
      if (document.getElementById('amountAndInterest2').style.display == 'block') {
       document.getElementById('amountAndInterest2').style.display = 'none';    
      }    
 };
 
-document.getElementById("remove3").onclick = function () {
+document.getElementById("minus-sign3").onclick = function () {
      if (document.getElementById('amountAndInterest3').style.display == 'block') {
       document.getElementById('amountAndInterest3').style.display = 'none';    
      }
 };
 
-
+// output div is hidden at first, starting date (today) is also hidden, although this should be adjustable in v2.0
 document.getElementById('output').style.visibility = 'hidden';
 document.getElementById('startDate').style.visibility = 'hidden';
 document.getElementById('startDate').valueAsDate = new Date();
@@ -43,69 +44,67 @@ document.getElementById('startDate').valueAsDate = new Date();
 
 
 function calc(){
+  // reveal the output divs
   document.getElementById('output').style.visibility = 'visible';
-  let interestRate = document.getElementById('rateInput0').value;
-  const rate = ((interestRate / 12) / 100);
-  const debt = document.getElementById("debtInput0").value;
-  
-  const freeDt = new Date(document.getElementById("freedomDate").value);
-  const totalDays = parseInt((freeDt - new Date) / (1000 * 60 * 60 * 24));
-  
-  const months = totalDays / 30.44;
 
-  // This is the loan amortization formula:
-  const monthlyPmt = parseFloat((rate * debt) / (1 - (Math.pow((1 + rate), (-months)))));
-
- 
-  document.getElementById('monthlyPayOutput').innerHTML = (monthlyPmt).toFixed(2);
-  
-  // The bottom 3 output divs:
-  document.getElementById('numdays').innerHTML = totalDays;
-  document.getElementById('numMonths').innerHTML = months.toFixed(2);
-  document.getElementById('numYears').innerHTML = (months / 12).toFixed(2);
-
-
-  function xtraCalc(d, r){
-
-  const xtraIntr = r.value;
-  console.log(xtraIntr + " xtraIntr");
-  const rt = ((xtraIntr / 12) / 100);
-  
-  const dbt = d.value;
-  console.log(dbt + " dbt");
-
+  // set up the date variables
   const freeDat = new Date(document.getElementById("freedomDate").value);
   const totalDys = parseInt((freeDat - new Date) / (1000 * 60 * 60 * 24));
-  console.log(totalDys + " totalDys");
-  const months = totalDays / 30.44;
-  console.log(months + " months");
+  const months = totalDys / 30.44;
 
-  const monthlyPay = parseFloat((rt * dbt) / (1 - (Math.pow((1 + rt), (-months)))));
+  // The calculation of any additional debts -- || 0 avoids displaying "NaN" before the user can enter both values
+  function xtraCalc(d, r){
+
+  const xtraIntr = r.value || 0;
+  const rt = ((xtraIntr / 12) / 100);
   
-  console.log(d);
-  console.log(d.id);
-  console.log(d.id.value);
+  const dbt = d.value || 0;
+
+  const monthlyPay = parseFloat((rt * dbt) / (1 - (Math.pow((1 + rt), (-months))))) || 0;
 
 
   return (monthlyPay).toFixed(2);
 
   }
 
-  var para1 = document.createElement("p");
-  para1.className = 'card bg-warning mb-2';
-  var para2 = document.createElement("p");
-  para2.className = 'card bg-warning mb-2';
-  var para3 = document.createElement("p");
-  para3.className = 'card bg-warning mb-2';
-  document.getElementById('monthlyPayOutput').appendChild(para1);
-  document.getElementsByClassName('card bg-warning mb-2')[1].innerHTML = "debt 2:" + " " + xtraCalc(Debt2, rate2); 
-  document.getElementById('monthlyPayOutput').appendChild(para2);
-  document.getElementsByClassName('card bg-warning mb-2')[2].innerHTML = "debt 3:" + " " + xtraCalc(Debt3, rate3)
-  document.getElementById('monthlyPayOutput').appendChild(para3);
-  document.getElementsByClassName('card bg-warning mb-2')[3].innerHTML = "debt 4:" + " " + xtraCalc(Debt4, rate4)
+
+  
+  // output the first debt payment
+  document.getElementById('monthlyPayOutput').innerHTML = xtraCalc(debtInput0, rateInput0);
+
+  // The bottom three days/months/years output divs:
+  document.getElementById('numdays').innerHTML = totalDys;
+  document.getElementById('numMonths').innerHTML = months.toFixed(2);
+  document.getElementById('numYears').innerHTML = (months / 12).toFixed(2);
+
+  // if any additional debts are added (made visible,) append to results div with the calculation
+  if (document.getElementById('amountAndInterest1').style.display == 'block') {
+      var para1 = document.createElement("p");
+      para1.className = 'card bg-warning mb-2';
+      document.getElementById('monthlyPayOutput').appendChild(para1);
+      document.getElementsByClassName('card bg-warning mb-2')[1].innerHTML = "debt 2:" + " " + xtraCalc(Debt2, rate2); 
+      console.log('Debt2 is ' + Debt2.value);   
+   }
+   if (document.getElementById('amountAndInterest2').style.display == 'block'){
+       var para2 = document.createElement("p");
+       para2.className = 'card bg-warning mb-2';
+       document.getElementById('monthlyPayOutput').appendChild(para2);
+       document.getElementsByClassName('card bg-warning mb-2')[2].innerHTML = "debt 3:" + " " + xtraCalc(Debt3, rate3)
+   }
+   if (document.getElementById('amountAndInterest3').style.display == 'block'){
+       var para3 = document.createElement("p");
+       para3.className = 'card bg-warning mb-2';;
+       document.getElementById('monthlyPayOutput').appendChild(para3);
+       document.getElementsByClassName('card bg-warning mb-2')[3].innerHTML = "debt 4:" + " " + xtraCalc(Debt4, rate4)
+   }
+  
+  
+  
+
 };
 
 
+// listen for any inputs and recalculate
 var inputs = document.querySelectorAll('input');
 inputs.forEach((inp) => {
   inp.addEventListener('input', calc)
