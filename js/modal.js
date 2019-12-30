a18c553014e688e5eeb39f290c23633c9faba163
studiozandra <zan@studiozandra.com>
2019-12-29 14:48:10 -1000

    add userInput file, loop

--- js/modal.js
+++ js/modal.js
@@ -5,6 +5,7 @@ var closeButton = document.querySelector(".closeButton");
 
 function totalDebts(){
   var total = (planOutput(debt1, rate1)) + ((planOutput(debt2, rate2)) || 0 ) + ((planOutput(debt3, rate3)) || 0 ) + ((planOutput(debt4, rate4)) || 0 );
+
   console.log(total);
   return total;
 }
@@ -14,7 +15,20 @@ function totalDebts(){
 // pop up modal
 function toggleModal() {
    modal.classList.toggle("show-modal");
+   console.log(allDebts.debt2.interest.value + " %")
    console.log(totalDebts);
+   // nested loop through userInput.js object
+   for (var key in allDebts){
+      if (allDebts.hasOwnProperty(key)){
+         for (var k in allDebts[key]){
+            if (allDebts[key].hasOwnProperty(k)){
+               console.log(key + " -> " + allDebts[key][k].value + ", " + allDebts[key][k].value)
+            
+            }
+         }
+          
+      }
+  }
    // next steps: total up the debts, if more than 1 (maybe value || 0), in the modal. Maybe a pie chart divided evenly with each radio checked
    // totalDebts = ( debt0 || 0) + ( debt1 || 0) + (debt2 || 0) + (debt3 || 0)
    // payoffPlan innerHTML = "To pay off " (totalDebts) + " by " + (user's chosen date) + ":"
---
+++ js/userInput.js
@@ -0,0 +1,21 @@
+var allDebts = {
+    debt1: {
+       debt: debt1,
+       interest: rate1
+    },
+    debt2: {
+       debt: debt2,
+       interest: rate2
+    },
+    debt3: {
+       debt: debt3,
+       interest: rate3
+    },
+    debt4: {
+       debt: debt4,
+       interest: rate4
+    }
+ };
+
+//  var totalAllDebts = allDebts.debt[i].debt.value
+
